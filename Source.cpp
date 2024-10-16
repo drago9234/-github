8b114588d776918fae9f478392b67b363e89276b
drago9234 <kostakosov79@gmail.com>
2024-10-16 20:37:04 +0300

    создал публичный класс и вывел его данные

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
+class tiles {
+public:
+	string brand;
+	int size_h;
+	int size_w;
+	float price;
+
+	void getData() {
+		cout << brand << " " << size_h << " " << size_w << " " << price << endl;
+	}
+
+};
+
+
 int main(){
-	cout << "hello world" << endl;
-	cout << "hello world2" << endl;
+	setlocale(0, "ru");
+	tiles t1;
+	tiles t2;
+	cout << "¬ведите t1: " << endl;
+	cin >> t1.brand >> t1.size_h >> t1.size_w >> t1.price;
+	cout << "¬ведите t2: " << endl;
+	cin >> t2.brand >> t2.size_h >> t2.size_w >> t2.price;
+	t1.getData();
+	t2.getData();
 	return 0;
 }
